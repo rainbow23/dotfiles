2405f509a6a3b81fe895fc4f5e171cbf11df2ee5
rainbow <info@rainbow23.jp>
2018-08-21 00:39:58 +0900

    add fzf-command fdr - cd to selected parent directory

--- init.sh
+++ init.sh
@@ -77,6 +77,21 @@ fd() {
   cd "$dir"
 }
 
+# fdp - cd to selected parent directory
+fdp() {
+  local declare dirs=()
+  get_parent_dirs() {
+    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
+    if [[ "${1}" == '/' ]]; then
+      for _dir in "${dirs[@]}"; do echo $_dir; done
+    else
+      get_parent_dirs $(dirname "$1")
+    fi
+  }
+  local DIR=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
+  cd "$DIR"
+}
+
 j() {
     if [[ "$#" -ne 0 ]]; then
         cd $(autojump $@)
