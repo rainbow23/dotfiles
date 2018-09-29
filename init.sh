6f3e4a48a2fdd5f0725d98e7983b12f62802a597
rainbow <info@rainbow23.jp>
2018-09-30 05:01:36 +0900

    install vim in shell script

--- init.sh
+++ init.sh
@@ -35,6 +35,42 @@ if [ ! -d $HOME/.anyenv/envs/pyenv ] ; then
   pyenv rehash
 fi
 
+case "${OSTYPE}" in
+darwin*)
+  # Mac
+  pyenv_gnu="~/.anyenv/envs/pyenv/versions/3.6.1/lib/python3.6/config-3.6m-darwin"
+  ;;
+linux*)
+  # Linux
+  pyenv_gnu="~/.anyenv/envs/pyenv/versions/3.6.1/lib/python3.6/config-3.6m-x86_64-linux-gnu"
+  ;;
+esac
+
+pyenv_path="~/.anyenv/envs/pyenv/versions/3.6.1/bin/python3.6"
+
+# /Users/goodscientist1023/.anyenv/envs/pyenv/versions/3.6.1/lib/python3.6/config-3.6m-darwin
+# /home/rainbow/.anyenv/envs/pyenv/versions/3.6.1/lib/python3.6/config-3.6m-x86_64-linux-gnu
+echo "$pyenv_path"
+echo "$pyenv_gnu"
+
+if [ ! -f /usr/local/bin/vim ] ; then
+  if [ ! -d $HOME/vim8src ] ; then
+    ¦git clone https://github.com/vim/vim.git ~/vim8src
+  fi
+
+  cd $HOME/vim8src && ./configure
+    --enable-fail-if-missing
+    --with-features=huge
+    --disable-selinux
+    --enable-luainterp
+    --enable-perlinterp
+    --enable-python3interp vi_cv_path_python3="$pyenv_path"
+    --with-python3-config-dir="$pyenv_gnu"
+    --enable-cscope
+    --enable-fontset
+    --enable-multibyte
+fi
+
 # fzf
 if [ ! -d $HOME/.fzf ] ; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
