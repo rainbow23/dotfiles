90f02eff6a604df4d00718bb872d2d997bd7a27d
rainbow <info@rainbow23.jp>
2018-12-20 00:03:46 +0900

    fix gsh

--- fzf.sh
+++ fzf.sh
@@ -40,6 +40,7 @@ gsh() {
   git log --graph --color=always \
       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
   fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "q:execute(echo {} | pbcopy)+abort" \
       --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
