59b963e2f9d31bd3a1eba3d263433957d33997c0
rainbow <info@rainbow23.jp>
2019-01-09 23:56:52 +0900

    fix install vim

--- install_vim.sh
+++ install_vim.sh
@@ -3,81 +3,16 @@ if [ ! -d $HOME/vim8src ]; then
   git clone --depth 1 https://github.com/vim/vim.git $HOME/vim8src
 fi
 
-# Get Linux distribution name
-get_os_distribution() {
-    if   [ -e /etc/debian_version ] ||
-         [ -e /etc/debian_release ]; then
-        # Check Ubuntu or Debian
-        if [ -e /etc/lsb-release ]; then
-            # Ubuntu
-            distri_name="ubuntu"
-        else
-            # Debian
-            distri_name="debian"
-        fi
-    elif [ -e /etc/redhat-release ]; then
-        # Red Hat Enterprise Linux
-        distri_name="redhat"
-    elif [ -e /etc/system-release-cpe ]; then
-        distri_name="amazonlinux"
-    elif [ -e /etc/arch-release ]; then
-        # Arch Linux
-        distri_name="arch"
-    elif echo ${OSTYPE} | grep -q "darwin"; then
-        # Mac
-        distri_name="darwin"
-    else
-        # Other
-        echo "unkown distribution"
-        distri_name="unkown"
-    fi
-
-    echo ${distri_name}
-}
-
-ostype=$(get_os_distribution)
+ostype=$($HOME/dotfiles/ostype.sh)
+./install_python.sh
 
 if [ $ostype = 'redhat' ] || [ $ostype = 'amazonlinux' ]; then
-  echo ""
-  echo "install ostype $ostype *************************************************"
-  echo ""
+  echo "" && "configure ostype $ostype ****************************************" && echo ""
 
   # error対応
   # undefined symbol: PyByteArray_Type
   # https://github.com/vim/vim/issues/3629
   export LDFLAGS="-rdynamic"
-
-  if [ ! -f /usr/local/bin/python3.5 ]; then
-    echo "install python3.5 *************************************************"
-    sudo ./install_python.sh
-    # sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-    # sudo yum -y install python36u  python36u-devel python36u-pip
-  fi
-
-elif [ $ostype = 'darwin' ]; then
-  echo ""
-  echo "install ostype $ostype *************************************************"
-  echo ""
-
-  brew install python3.7
-  # fi
-    # sudo pip install --upgrade pip
-    # sudo python3.5 -m pip install --upgrade pip
-    # sudo python3.5 -m pip install neovim
-    # sudo python3.5 -m pip install --user pynvim
-    # sudo python3.5 -m pip install --upgrade pynvim
-    # pip install --user neovim
-fi
-
-pip3 install --upgrade pip
-pip3 install neovim
-pip3 install --user neovim
-pip3 install pynvim
-
-if [ $ostype = 'redhat' ] || [ $ostype = 'amazonlinux' ]; then
-  echo ""
-  echo "configure ostype $ostype *************************************************"
-  echo ""
 
   cd $HOME/vim8src && ./configure\
     --enable-fail-if-missing\
@@ -91,12 +26,10 @@ if [ $ostype = 'redhat' ] || [ $ostype = 'amazonlinux' ]; then
     --enable-cscope\
     --enable-fontset\
     --enable-multibyte
-elif [ $ostype = 'darwin' ]; then
-  echo ""
-  echo "configure ostype $ostype *************************************************"
-  echo ""
 
-# `brew --prefix` >> /usr/local
+elif [ $ostype = 'darwin' ]; then
+  echo "" && "configure ostype $ostype ****************************************" && echo ""
+  # `brew --prefix` >> /usr/local
   cd $HOME/vim8src && ./configure\
     --prefix=`brew --prefix`\
     --enable-fail-if-missing\
