048b18ea8d827d9a36a11b50b81966c9a3e68cc1
rainbow <info@rainbow23.jp>
2019-01-14 03:28:52 +0900

    fixed error symlind.sh

--- symlink.sh
+++ symlink.sh
@@ -4,4 +4,6 @@ ln -sfn $HOME/dotfiles/_vimrc $HOME/.vimrc
 ln -sfn $HOME/dotfiles/_bashrc $HOME/.bashrc
 ln -sfn $HOME/dotfiles/_zshrc $HOME/.zshrc
 ln -sfn $HOME/dotfiles/_tmux.conf $HOME/.tmux.conf
+mkdir -p -m 744 $HOME/.config/nvim
 ln -sfn $HOME/dotfiles/_vimrc $HOME/.config/nvim/init.vim
+
