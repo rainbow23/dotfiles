d4fc22870f3b839301bc23019c7b538b6cb5e12d
rainbow <info@rainbow23.jp>
2019-03-17 21:46:05 +0900

    install git

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 sudo chsh $USER -s /bin/zsh 2>/dev/null
 INSTALL_DIR=$HOME/dotfiles
 
+yum -y install git
+
 if [[ ! -d $INSTALL_DIR ]]; then
   git clone https://github.com/rainbow23/dotfiles.git $INSTALL_DIR
 fi
