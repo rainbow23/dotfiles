de9b7c2ee0ad377ff38b8e1b54d3a9de9dfc3806
rainbow <info@rainbow23.jp>
2019-03-17 17:00:17 +0900

    fixed init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,7 @@ sudo chsh $USER -s /bin/zsh 2>/dev/null
 INSTALL_DIR=$HOME/dotfiles
 
 if [[ ! -d $INSTALL_DIR ]]; then
-  mkdir -p $INSTALL_DIR && cd $INSTALL_DIR
-  git clone https://github.com/rainbow23/dotfiles.git
-# git checkout develop
+  git clone https://github.com/rainbow23/dotfiles.git $INSTALL_DIR
 fi
 
 cd $INSTALL_DIR
