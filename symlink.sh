6f20330a8b4630f8c16b6215e111626ddf4ef17a
rainbow <info@rainbow23.jp>
2019-09-29 20:56:26 +0900

    fix symlink

--- symlink.sh
+++ symlink.sh
@@ -5,6 +5,8 @@ ln -sfn $HOME/dotfiles/_bashrc $HOME/.bashrc
 ln -sfn $HOME/dotfiles/_zshrc $HOME/.zshrc
 ln -sfn $HOME/dotfiles/_tmux.conf $HOME/.tmux.conf
 ln -sfn $HOME/dotfiles/_tmuxp  $HOME/.tmuxp
-mkdir -p -m 744 $HOME/.config/nvim
+if [ ! -d $HOME/.config/nvim ];then
+    mkdir -p -m 744 $HOME/.config/nvim
+fi
 ln -sfn $HOME/dotfiles/_vimrc $HOME/.config/nvim/init.vim
 
