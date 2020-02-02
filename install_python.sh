e695231a1c3662d1a9c2f1c35926fa1e50d9ae0f
rainbow23 <info@rainbow23.jp>
2020-02-02 23:11:43 +0900

    fixed python3 install set up in ubuntu

--- install_python.sh
+++ install_python.sh
@@ -10,15 +10,14 @@ if [ $ostype = 'darwin' ]; then
   brew install python3
 fi
 
-if [ $ostype = 'redhat' ] || [ $ostype = 'amazonlinux' ]; then
-
+if [ $ostype = 'ubuntu' ] || [ $ostype = 'redhat' ] || [ $ostype = 'amazonlinux' ]; then
 PYTHON_INSTALL_DIR=$HOME/python
   if [ ! -d $PYTHON_INSTALL_DIR ]; then
     mkdir $PYTHON_INSTALL_DIR
     cd $PYTHON_INSTALL_DIR
-    wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
-    tar zxvf Python-3.6.0.tgz
-    cd Python-3.6.0
+    wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
+    tar zxvf Python-3.6.1.tgz
+    cd Python-3.6.1
     sudo yum -y install zlib-devel openssl-devel tk-devel
     sudo ./configure --prefix=/usr/local/Python36
     sudo make
@@ -26,18 +25,17 @@ PYTHON_INSTALL_DIR=$HOME/python
     sudo chown -R ${USERNAME}:${USERNAME} /usr/local/Python36
   fi
 
-  if [ -L /usr/local/bin/python3.6 ]; then
-      sudo rm -rf /usr/local/bin/python3.6
+  if [ -L /usr/local/bin/python3 ]; then
+      sudo rm -rf /usr/local/bin/python3
   fi
   if [ -L /usr/local/bin/pip3 ]; then
       sudo rm -rf /usr/local/bin/pip3
   fi
 
-  sudo ln -s /usr/local/Python36/bin/python3.6 /usr/local/bin/python3.6
-  sudo ln -s /usr/local/Python36/bin/pip3.6 /usr/local/bin/pip3
+sudo ln -s /usr/local/Python36/bin/python3.6.1 /usr/local/bin/python3
+sudo ln -s /usr/local/Python36/bin/pip3.6.1 /usr/local/bin/pip3
 fi
 
-pip3 --no-cache-dir install --upgrade pip
-pip3 --no-cache-dir install neovim
+pip3 --no-cache-dir install --upgrade --user pip
 pip3 --no-cache-dir install --user neovim
-pip3 --no-cache-dir install pynvim
+pip3 --no-cache-dir install --user pynvim
