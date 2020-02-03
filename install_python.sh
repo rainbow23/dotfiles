1a87e6b06dad5de59d790f0974b9f21d3ddf5007
rainbow <info@rainbow23.jp>
2020-02-03 22:59:53 +0900

    fixed python install

--- install_python.sh
+++ install_python.sh
@@ -11,31 +11,31 @@ if [ $ostype = 'darwin' ]; then
 fi
 
 if [ $ostype = 'ubuntu' ] || [ $ostype = 'redhat' ] || [ $ostype = 'amazonlinux' ]; then
-PYTHON_INSTALL_DIR=$HOME/python
+  PYTHON_INSTALL_DIR=$HOME/python
   if [ ! -d $PYTHON_INSTALL_DIR ]; then
     mkdir $PYTHON_INSTALL_DIR
     cd $PYTHON_INSTALL_DIR
-    wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
-    tar zxvf Python-3.6.1.tgz
-    cd Python-3.6.1
+    wget https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
+    tar zxvf Python-3.5.0.tgz
+    cd Python-3.5.0
     sudo yum -y install zlib-devel openssl-devel tk-devel
-    sudo ./configure --prefix=/usr/local/Python36
+    sudo ./configure --prefix=/usr/local/Python35
     sudo make
     sudo make install
-    sudo chown -R ${USERNAME}:${USERNAME} /usr/local/Python36
+    sudo chown -R ${USERNAME}:${USERNAME} /usr/local/Python35
   fi
 
-  if [ -L /usr/local/bin/python3 ]; then
-      sudo rm -rf /usr/local/bin/python3
+  if [ -L /usr/local/bin/python3.5 ]; then
+      sudo rm -rf /usr/local/bin/python3.5
   fi
   if [ -L /usr/local/bin/pip3 ]; then
       sudo rm -rf /usr/local/bin/pip3
   fi
 
-sudo ln -s /usr/local/Python36/bin/python3.6.1 /usr/local/bin/python3
-sudo ln -s /usr/local/Python36/bin/pip3.6.1 /usr/local/bin/pip3
+  sudo ln -s /usr/local/Python35/bin/python3.5 /usr/local/bin/python3
+  sudo ln -s /usr/local/Python35/bin/pip3.5 /usr/local/bin/pip3
 fi
 
-pip3 --no-cache-dir install --upgrade --user pip
+pip3 --no-cache-dir install --user --upgrade pip
 pip3 --no-cache-dir install --user neovim
 pip3 --no-cache-dir install --user pynvim
