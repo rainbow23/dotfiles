bfc45518b01406ccc25c8429a101660c2ed571e2
rainbow23 <info@rainbow23.jp>
2022-11-23 21:00:57 +0900

    change status color of tmux pop up

--- etc/popuptmux.sh
+++ etc/popuptmux.sh
@@ -1,5 +1,9 @@
 if [ "$(tmux display-message -p -F "#{session_name}")" = "popup"  ];then
+    tmux set-option -g status-bg 'colour218'
     tmux detach-client
   else
-    tmux popup -E -w100% -h98% 'tmux attach -t popup || tmux new -s popup'
+    # color list
+    # https://qiita.com/nojima/items/9bc576c922da3604a72b
+    tmux set-option -g status-bg 'colour50'
+    tmux popup -E -w100% -h99% 'tmux attach -t popup || tmux new -s popup'
 fi
