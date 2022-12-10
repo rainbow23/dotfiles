cd75a3f17c36d5752d438cf158f980305f76dab7
rainbow23 <info@rainbow23.jp>
2022-12-10 12:30:34 +0900

    cd git root dir when move to popup window

--- etc/popuptmux.sh
+++ etc/popuptmux.sh
@@ -2,8 +2,15 @@ if [ "$(tmux display-message -p -F "#{session_name}")" = "popup"  ];then
     tmux set-option -g status-bg 'colour218'
     tmux detach-client
   else
+    rootDir=$(git rev-parse --show-toplevel)
+    if [ ! -z "$rootDir" ]; then
+      # セッション切り替え後にコマンドを送信すると実行できない場合があるので、セッションを切り替える前に送信する
+      tmux send-keys -t 'popup' "cd $rootDir ; clear" 'C-m'
+    fi
+
     # color list
     # https://qiita.com/nojima/items/9bc576c922da3604a72b
     tmux set-option -g status-bg 'colour50'
+    # tmux new -s -> Start New Session With Name
     tmux popup -E -w100% -h99% 'tmux attach -t popup || tmux new -s popup'
 fi
