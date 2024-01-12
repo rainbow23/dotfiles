4bbdc1d62092735c987ce955e5d0c0964c6c9228
rainbow23 <info@rainbow23.jp>
2024-01-12 23:07:22 +0900

    tmux popupでインタラクティブシェルのカレントディレクトリに移動が失敗する不具合対応

--- etc/popuptmux.sh
+++ etc/popuptmux.sh
@@ -1,7 +1,14 @@
+#!/bin/sh
+
+# 第1引数を変数に格納
+currentDir="$1"
+# echo "Received current directory: $currentDir"
+
 if [ "$(tmux display-message -p -F "#{session_name}")" = "popup"  ];then
     tmux set-option -g status-bg 'colour218'
     tmux detach-client
   else
+    cd $currentDir  #tmuxのインタラクティブシェルのディレクトリに移動
     rootDir=$(git rev-parse --show-toplevel)
     if [ ! -z "$rootDir" ]; then
       # セッション切り替え後にコマンドを送信すると実行できない場合があるので、セッションを切り替える前に送信する
