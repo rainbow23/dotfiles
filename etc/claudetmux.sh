112f93d32ad317d4866f7587aaebb8f71d2dce3b
rainbow23 <info@rainbow23.jp>
2026-04-16 10:45:45 +0900

    claudeセッションの作成はcurrent directoryに紐づけていたため、sub directoryごとにclaudeセッションが作成されて煩雑になっていた。 gitのルートディレクトリに対して１つのclaudeセッションを作成するように修正した

--- etc/claudetmux.sh
+++ etc/claudetmux.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 currentDir="$1"
-SESSION="z-claude-$(echo "$currentDir" | md5sum | cut -c1-8)"
+rootDir=$(git -C "$currentDir" rev-parse --show-toplevel 2>/dev/null)
+
+if [ -z "$rootDir" ]; then
+  rootDir="$currentDir"
+fi
+
+SESSION="z-claude-$(echo "$rootDir" | md5sum | cut -c1-8)"
 
 if [ "$(tmux display-message -p -F '#{session_name}')" = "$SESSION" ]; then
     tmux detach-client
 else
     tmux has-session -t "$SESSION" 2>/dev/null || \
-    tmux new-session -d -s "$SESSION" -c "$currentDir" "claude"
+    tmux new-session -d -s "$SESSION" -c "$rootDir" "claude"
     tmux display-popup -w80% -h80% -E "tmux attach-session -t $SESSION"
     tmux set-option -t "$SESSION" status-bg "colour210"
 fi
